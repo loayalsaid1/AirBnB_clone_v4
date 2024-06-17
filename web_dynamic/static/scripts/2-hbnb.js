6ce706002d16fec1d4b873c925cba81e0692f254
Loay Al-Said <l.programming555@gmail.com>
2024-06-17 03:05:59 +0300

    Add task 4

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     $('.amenities h4').text(names.join(', '));
   });
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
